aa1b3c2e224280d4ba6895d200617c1ef74518a7
adam <adam@theobsidiancollection.org>
2019-11-06 13:04:06 -0500

    initial functionality finished just needs styling added active storage

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -69,6 +69,6 @@ class ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params.require(:image).permit(:title, :description, :date_created, :price, :collection)
+      params.require(:image).permit(:title, :photo, :description, :date_created, :price, :collection)
     end
 end
